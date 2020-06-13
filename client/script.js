fefaf0ac1b3b2bc210fcab6d20cf6273c39f3c77
Valentin Rebscher <valentin.rebscher@yatta.de>
2020-06-13 14:45:01 +0200

    update client design

--- client/script.js
+++ client/script.js
@@ -8,8 +8,23 @@ function color(red, green, blue) {
     type: 'PUT',
     url: '/color'
   }).done(data => {
-    console.log('color changed');
+    console.log('color changed')
   })
+  let hexRed = red.toString(16).length == 1 ? '0' + red.toString(16) : red.toString(16)
+  let hexGreen = green.toString(16).length == 1 ? '0' + green.toString(16) : green.toString(16)
+  let hexBlue = blue.toString(16).length == 1 ? '0' + blue.toString(16) : blue.toString(16)
+  let hex = '#' + hexRed + hexGreen + hexBlue
+  document.querySelector('.color-container-outter').style.background = hex
+
+}
+
+function customColor() {
+  let hex = document.querySelector('#color-picker').value
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  let r = parseInt(result[1], 16)
+  let g = parseInt(result[2], 16)
+  let b = parseInt(result[3], 16)
+  color(r, g, b)
 }
 
 function wipe(red, green, blue) {
@@ -22,7 +37,7 @@ function wipe(red, green, blue) {
     type: 'PUT',
     url: '/wipe'
   }).done(data => {
-    console.log('color wipe started');
+    console.log('color wipe started')
   })
 }
 
@@ -32,7 +47,7 @@ function twinkleRandom() {
     type: 'PUT',
     url: '/twinkle-random'
   }).done(data => {
-    console.log('twinkle random started');
+    console.log('twinkle random started')
   })
 }
 
@@ -42,6 +57,6 @@ function runningLights() {
     type: 'PUT',
     url: '/running-lights'
   }).done(data => {
-    console.log('running lights started');
+    console.log('running lights started')
   })
 }
